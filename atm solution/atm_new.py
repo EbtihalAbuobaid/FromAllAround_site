d9635f25694d0bf068af748d38e346ce0e605f8f
Ebtihal Abu Obaid <ebtihal.abuobaid@gmail.com>
2018-02-21 11:18:42 +0200

    ATM function

---
+++ atm solution/atm_new.py
@@ -0,0 +1,41 @@
+
+def withdraw(balance, value):
+	print "Current balance = " + str(balance)
+	current_balance =  balance- value
+	if   value > balance:
+		print "Can't give you all this money !!"
+
+	elif value < 0:
+		print "More than zero plz!" 
+
+	else:
+		while value > 0:
+        
+			if value >= 100:
+				value -= 100
+				print "give 100"
+
+			elif value >= 50:
+				value -= 50
+				print "give 50"
+
+			elif value >= 10:
+				value -= 10
+				print "give 10"
+			elif value >= 5:
+				value -= 5
+				print("give 5")
+
+			elif value < 5:
+				print("give " + str(value))
+				value = 0
+	balance= current_balance
+	return balance
+	
+	
+
+balance = 500
+balance = withdraw(balance, 277)
+balance = withdraw(balance, 30)
+balance = withdraw(balance, 5)
+balance = withdraw(balance, 500)
